dc57032a08dcccacc3c3479ac3640aae663ae9b3
adam <samuelson93@gmail.com>
2024-10-17 12:16:28 +0200

    shared websocket between GS and logs melloware@react-logviewer component added for displaying log.

--- src/components/gameViewer/bottomBar/BottomBar.tsx
+++ src/components/gameViewer/bottomBar/BottomBar.tsx
@@ -3,27 +3,11 @@ import './BottomBar.css';
 // import LogToolbar from './LogToolbar';
 import { LazyLog, ScrollFollow } from "@melloware/react-logviewer";
 
-interface BottomBarProps {}
+type Log = Record<string, any>;
 
-// wrap string in ANSI color codes
-function ct(color, text) {
-    const colors = {
-        black: '\x1b[30m',
-        red: '\x1b[31m',
-        green: '\x1b[32m',
-        yellow: '\x1b[33m',
-        blue: '\x1b[34m',
-        magenta: '\x1b[35m',
-        cyan: '\x1b[36m',
-        white: '\x1b[37m',
-        reset: '\x1b[39m' // Resets the text color to default
-    };
+interface BottomBarProps {logs: Log[]}
 
-    const colorCode = colors[color.toLowerCase()] || colors.reset;
-    return `${colorCode}${text}${colors.reset}`;
-}
-
-const BottomBar: React.FC<BottomBarProps> = () => {
+const BottomBar: React.FC<BottomBarProps> = ({logs}) => {
 
 
     return (
@@ -32,15 +16,13 @@ const BottomBar: React.FC<BottomBarProps> = () => {
             startFollowing={true}
             render={({ follow, onScroll }) => (
             <LazyLog
-                url="ws://localhost:8080/logs"
-                stream
-                websocket
                 caseInsensitive
                 enableSearch
                 selectableLines
                 onScroll={onScroll}
                 follow={follow}
                 enableHotKeys
+                text={logs.map((log) => JSON.stringify(log)).join('\n')}
             />
             )}
         />
