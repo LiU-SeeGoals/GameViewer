ef697332cb4dcfe5186f83dea178273258612cf0
Sebastian Olsson <sg.olsson@hotmail.se>
2025-01-25 17:26:16 +0100

    Update Action type to match controller

--- src/types/Action.ts
+++ src/types/Action.ts
@@ -1,5 +1,13 @@
 export type Action = {
-    Id: number;
-    Action: number;
+    Id:         number;
+    Action:     number;
+    PosX:       number;
+    PosY:       number;
+    PosW:       number;
+    DestX:      number;
+    DestY:      number;
+    DestW:      number;
+    Dribble:    boolean;
     PreviousAction: number;
+
 }
