d8393ce123d0b84b5220404d2ce9553e35c7a962
Rasmus Wallin <rasmus.wallin@foi.se>
2025-03-16 23:57:52 +0100

    set host/port from envvars

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Listen on all network interfaces
+    host: process.env.GAME_VIEWER_ADDR,
+    port: parseInt(process.env.GAME_VIEWER_PORT),
   },
 });
